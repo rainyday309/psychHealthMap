aa39fc1ff968762dd7574dab5ac2253b3b7d0e75
Wei-Cheng Yang <rainyday309@gmail.com>
2016-04-09 23:21:48 +0800

    ADD: add table below with information of rehabilitation centers

--- app/public/js/listItem.js
+++ app/public/js/listItem.js
@@ -1,10 +1,25 @@
 $.getJSON("public/json/rehab.json")
 .done(function(data){
-	
-	/* 
-	console.log('data retrieved');
-	console.log(data);
-	*/
+
+	/* handle each object by appending into table */
+	$.each(data, function(key,val){
+
+		/* add center data to the table */
+		$("<tr></tr>").append("<td>"+val['id']+"</td>")
+									.append("<td>"+val['name']+"</td>")
+								  .append("<td>"+val['address']+"</td>")
+								  .append("<td>"+val['tel']+"</td>")
+								  .attr('id', 'center' + val['id'])
+								  .appendTo("#contact");
+		/* add marker to the map 
+
+		L.marker(val['coordinate'], {
+			title: val['name']
+		});
+*/
+
+	});
+
 })
 .fail(function(){
 	$('.list').append('<p>Failed to retrieve data from server!</p>');
