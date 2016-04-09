6a730c40fb8ac82f3c01b94348c370b38c459f8b
Wei-Cheng Yang <rainyday309@gmail.com>
2016-04-09 21:43:23 +0800

    ADD: rehab.json: data from pdf to json

--- app/public/js/drawmap.js
+++ app/public/js/drawmap.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([22.6169,120.2960],13);
+var mymap = L.map('mapid').setView([22.62595,120.32554],17);
 
 L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
 	{
